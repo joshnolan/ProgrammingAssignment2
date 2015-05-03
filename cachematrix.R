13e78e5f73d17a676d3600c0446712d9b65ec56f
Josh Nolan <josh@boldarray.com>
2015-05-03 12:10:53 -0700

    assignment completed

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y)
+  {
+    x <<- y
+    i <- NULL
+  }
+  get <- function() x
+  set_i <- function(inverse) i <<- solve
+  get_i <- function() i
+  list(set = set, get = set, set_i = set_i, get_i = get_i)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  i <- x$get_i()
+  if(!is.null(i)) {
+    message("gettomg cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$set_i(i)
+  i
+  
 }
